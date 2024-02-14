074c3b553647cb3407ad0b73c3a747a4daba62c1
mpociot <mpociot@users.noreply.github.com>
2024-02-14 08:46:26 +0000

    Fix styling

--- src/Oracle.php
+++ src/Oracle.php
@@ -59,7 +59,7 @@ class Oracle
         return $completions->choices[0]->text;
     }
 
-    protected function buildPrompt(string $question, string $query = null, string $result = null): string
+    protected function buildPrompt(string $question, ?string $query = null, ?string $result = null): string
     {
         $tables = $this->getTables($question);
 
